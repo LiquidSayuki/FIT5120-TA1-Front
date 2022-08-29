a3e98cbbf729a5418973eb83bb0de1a9ca16ce55
LiquidSayuki <liquidsayuki@icloud.com>
2022-08-29 19:49:47 +1000

    Typesetting for every single disease pages.

--- src/views/disease/SingleDisease.js
+++ src/views/disease/SingleDisease.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {Col, Layout, Row, List,Button, Typography, Divider} from 'antd';
+import {Col, Layout, Row, Image,Button, Typography, Divider, Card} from 'antd';
 import LocationIdentifier from "../../components/basicPageFrame/LocationIdentifier";
 import axios from "axios";
 
-const {Content} = Layout;
+const { Meta } = Card;
+const { Content } = Layout;
+const { Title, Paragraph } = Typography;
 
 // const data = {
 //     id:"01",
@@ -32,7 +34,57 @@ const SingleDisease = (props) => {
         >
             <LocationIdentifier title={data.name}/>
             <div className="site-layout-content">
-                <div>{data.name}</div>
+                <Row style={{paddingTop:"50px"}}>
+                    <Col span={3}></Col>
+                    <Col span={18}>
+                        <Card style={{width:"100%"}}>
+                            <Typography>
+                                <Title>{data.name}</Title>
+                                <Divider/>
+                                <Paragraph style={{fontSize:"18px"}}>
+                                    {data.content}
+                                </Paragraph>
+                            </Typography>
+                        </Card>
+                    </Col>
+
+                </Row>
+                <Row style={{paddingTop:"50px"}}>
+                    <Col span={3}></Col>
+                    <Col span={10}>
+                        <Card
+                            title="Symptom"
+                            style={{
+                                width: "95%",
+                            }}
+                        >
+                            <blockquote>{data.symptom}</blockquote>
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card
+                            hoverable
+                            cover={<Image alt={data.name + "Image"} src={data.imgSrc}/>}
+                        >
+                            <Meta description="Pediroo" />
+                        </Card>
+                    </Col>
+                    <Col span={3}></Col>
+                </Row>
+                <Row style={{paddingTop:"50px"}}>
+                    <Col span={3}></Col>
+                    <Col span={18}>
+                        <Card
+                            title="Prevention"
+                            style={{
+                                width: "100%",
+                            }}
+                        >
+                            <blockquote>{data.prevention}</blockquote>
+                        </Card>
+                    </Col>
+                    <Col span={3}></Col>
+                </Row>
             </div>
         </Content>
     );
