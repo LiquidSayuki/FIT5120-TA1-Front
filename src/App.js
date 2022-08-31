a0e50638afd2e84dd16fd96048cd26bb2eb9056e
LiquidSayuki <liquidsayuki@icloud.com>
2022-08-31 23:18:11 +1000

    Apply login feature

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import IndexRouter from "./router/IndexRouter";
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import React from "react";
 import Login from "./views/login/Login";
+import LoginSuccess from "./views/login/LoginSuccess";
 
 function App() {
   return (
       <BrowserRouter>
           <Switch>
+              <Route path="/loginSuccess" component={LoginSuccess} />
               <Route path="/login" component={Login} />
               <Route path="/"
                      render={()=>
