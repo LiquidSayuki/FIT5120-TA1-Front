60befa3cf6802a2bea3fcc0c15558809df70076a
rwan0085 <rwan0085@student.monash.edu>
2022-08-31 23:36:45 +1000

    login callback url modified

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -3,7 +3,7 @@ import { Button, Result } from 'antd';
 
 const Login = (props) => {
 
-    const cognitalURL = "https://ie-ta01.auth.ap-southeast-2.amazoncognito.com/login?client_id=7k2qpqrk7eg5ujru3dlobl9b11&response_type=code&scope=openid+profile&redirect_uri=http://localhost:3000/home"
+    const cognitalURL = "https://ie-ta01.auth.ap-southeast-2.amazoncognito.com/login?client_id=7k2qpqrk7eg5ujru3dlobl9b11&response_type=code&scope=openid+profile&redirect_uri=https://pediroo.me/loginSuccess"
 
     const onClick = () => {
         props.history.push("/home")
