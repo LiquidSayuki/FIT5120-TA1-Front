a788c9c7fa3316526f8fd272f453a2487f0ab8a5
rwan0085 <rwan0085@student.monash.edu>
2022-09-01 12:28:53 +1000

    No changing but requires to commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import IndexRouter from "./router/IndexRouter";
-import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import React from "react";
 import Login from "./views/login/Login";
 import LoginSuccess from "./views/login/LoginSuccess";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Switch>
-              <Route path="/loginSuccess" component={LoginSuccess} />
-              <Route path="/login" component={Login} />
-              <Route path="/"
-                     render={()=>
-                         localStorage.getItem('access-token')?(<IndexRouter/>) : (<Redirect to="/login"/>)
-              }/>
-          </Switch>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/loginSuccess" component={LoginSuccess} />
+                <Route path="/login" component={Login} />
+                <Route path="/"
+                    render={() =>
+                        localStorage.getItem('access-token') ? (<IndexRouter />) : (<Redirect to="/login" />)
+                    } />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
