9c7d1c16caf74a791d72bacdc4fe21eeda8dffdf
rwan0085 <rwan0085@student.monash.edu>
2022-09-05 20:35:54 +1000

    Vaccine data can collapse by age & Install '@wangeditor' locally

--- src/views/vaccine/BirthToAges4.js
+++ src/views/vaccine/BirthToAges4.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Layout, Timeline, Card, Col, Row, Divider, Typography, Checkbox } from 'antd';
+import { Layout, Timeline, Card, Col, Row, Divider, Typography, Checkbox, Collapse } from 'antd';
 import LocationIdentifier from "../../components/basicPageFrame/LocationIdentifier";
 import './BirthToAges4.css'
 import axios from "axios";
 
 const { Content } = Layout;
+const { Panel } = Collapse;
 const { Title, Paragraph } = Typography;
 
 const title = "Children Immunization"
@@ -49,148 +50,112 @@ const BirthToAges6 = () => {
                     <div className='vaccineContent' style={{ display: 'flex', justifyContent: 'space-between', paddingTop: "0.5rem" }}>
                         <div className='vaccinations' style={{ justifyContent: 'center' }}>
 
+
                             {/* vaccine data for children once birth */}
                             <div style={{ paddingTop: "30px" }}>
-                                <h2>Birth</h2>
-                                <div className='grid-container'>
-                                    {vaccines.map(item => item.childrenAge === 'Birth' ? //<--HERE  
-                                        <Card
-                                            key={item.id}
-                                            title={item.name}
-                                            // extra={<a href="#">More</a>}
-                                            style={{
-                                                width: 300,
-                                            }}>
-                                            <p>{item.content}</p>
-                                            {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
-                                        </Card>
-                                        : null)}
-                                </div>
+                                <Collapse bordered={false} defaultActiveKey={['1']}>
+                                    <Panel header=<h2>Birth</h2> key="1">
+                                        <div className='grid-container'>
+                                            {vaccines.map(item => item.childrenAge === 'Birth' ? //<--HERE  
+                                                <Card
+                                                    key={item.id}
+                                                    title={item.name}
+                                                    // extra={<a href="#">More</a>}
+                                                    style={{
+                                                        width: 300,
+                                                    }}>
+                                                    <p>{item.content}</p>
+                                                    {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
+                                                </Card>
+                                                : null)}
+                                        </div>
+                                    </Panel>
+                                </Collapse>
                             </div>
 
-                            {/* vaccine data for children at 2 months */}
+                            {/* vaccine data for children at 2-6 months */}
                             <div style={{ paddingTop: "30px" }}>
-                                <h2>2 months</h2>
-                                <div className='grid-container'>
-                                    {vaccines.map(item => item.childrenAge === '2 months' ? //<--HERE  
-                                        <Card
-                                            key={item.id}
-                                            title={item.name}
-                                            // extra={<a href="#">More</a>}
-                                            style={{
-                                                width: 300,
-                                            }}>
-                                            <p>{item.content}</p>
-                                            {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
-                                        </Card>
-                                        : null)}
-                                </div>
-                            </div>
-
-                            {/* vaccine data for children at 4 months */}
-                            <div style={{ paddingTop: "30px" }}>
-                                <h2>4 months</h2>
-                                <div className='grid-container'>
-                                    {vaccines.map(item => item.childrenAge === '4 months' ? //<--HERE  
-                                        <Card
-                                            key={item.id}
-                                            title={item.name}
-                                            // extra={<a href="#">More</a>}
-                                            style={{
-                                                width: 300,
-                                            }}>
-                                            <p>{item.content}</p>
-                                            {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
-                                        </Card>
-                                        : null)}
-                                </div>
-                            </div>
-
-                            {/* vaccine data for children at 6 months */}
-                            <div style={{ paddingTop: "30px" }}>
-                                <h2>6 months</h2>
-                                <div className='grid-container'>
-                                    {vaccines.map(item => item.childrenAge === '6 months' ? //<--HERE  
-                                        <Card
-                                            key={item.id}
-                                            title={item.name}
-                                            // extra={<a href="#">More</a>}
-                                            style={{
-                                                width: 300,
-                                            }}>
-                                            <p>{item.content}</p>
-                                            {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
-                                        </Card>
-                                        : null)}
-                                </div>
+                                {/* <h2>2-6 months</h2> */}
+                                <Collapse bordered={false} defaultActiveKey={['1']}>
+                                    <Panel header=<h2>2-6 months</h2> key="1">
+                                        <div className='grid-container'>
+                                            {vaccines.map(item => item.childrenAge === '2 months' ? //<--HERE  
+                                                <Card
+                                                    key={item.id}
+                                                    title={item.name}
+                                                    // extra={<a href="#">More</a>}
+                                                    style={{
+                                                        width: 300,
+                                                    }}>
+                                                    <p>{item.content}</p>
+                                                    {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
+                                                </Card>
+                                                : null)}
+                                        </div>
+                                    </Panel>
+                                </Collapse>
                             </div>
 
                             {/* vaccine data for children at 12 months */}
                             <div style={{ paddingTop: "30px" }}>
-                                <h2>12 months</h2>
-                                <div className='grid-container'>
-                                    {vaccines.map(item => item.childrenAge === '12 months' ? //<--HERE  
-                                        <Card
-                                            key={item.id}
-                                            title={item.name}
-                                            // extra={<a href="#">More</a>}
-                                            style={{
-                                                width: 300,
-                                            }}>
-                                            <p>{item.content}</p>
-                                            {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
-                                        </Card>
-                                        : null)}
-                                </div>
+                                <Collapse bordered={false} defaultActiveKey={['1']}>
+                                    <Panel header=<h2>12 months</h2> key="1">
+                                        <div className='grid-container'>
+                                            {vaccines.map(item => item.childrenAge === '12 months' ? //<--HERE  
+                                                <Card
+                                                    key={item.id}
+                                                    title={item.name}
+                                                    // extra={<a href="#">More</a>}
+                                                    style={{
+                                                        width: 300,
+                                                    }}>
+                                                    <p>{item.content}</p>
+                                                    {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
+                                                </Card>
+                                                : null)}
+                                        </div>
+                                    </Panel>
+                                </Collapse>
                             </div>
 
                             {/* vaccine data for children at 18 months */}
                             <div style={{ paddingTop: "30px" }}>
-                                <h2>18 months</h2>
-                                <div className='grid-container'>
-                                    {vaccines.map(item => item.childrenAge === '18 months' ? //<--HERE  
-                                        <Card
-                                            key={item.id}
-                                            title={item.name}
-                                            // extra={<a href="#">More</a>}
-                                            style={{
-                                                width: 300,
-                                            }}>
-                                            <p>{item.content}</p>
-                                            {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
-                                        </Card>
-                                        : null)}
-                                </div>
+                                <Collapse bordered={false} defaultActiveKey={['1']}>
+                                    <Panel header=<h2>18 months</h2> key="1">
+                                        <div className='grid-container'>
+                                            {vaccines.map(item => item.childrenAge === '18 months' ? //<--HERE  
+                                                <Card
+                                                    key={item.id}
+                                                    title={item.name}
+                                                    // extra={<a href="#">More</a>}
+                                                    style={{
+                                                        width: 300,
+                                                    }}>
+                                                    <p>{item.content}</p>
+                                                    {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
+                                                </Card>
+                                                : null)}
+                                        </div>
+                                    </Panel>
+                                </Collapse>
                             </div>
 
                             {/* vaccine data for children at 4 years */}
                             <div style={{ paddingTop: "30px" }}>
-                                <h2>4 years</h2>
-                                <div className='grid-container'>
-                                    {vaccines.map(item => item.childrenAge === '4 years' ? //<--HERE  
-                                        <Card
-                                            key={item.id}
-                                            title={item.name}
-                                            // extra={<a href="#">More</a>}
-                                            style={{
-                                                width: 300,
-                                            }}>
-                                            <p>{item.content}</p>
-                                            {/* <Checkbox onChange={onChange}>Inoculated</Checkbox> */}
-                                        </Card>
-                                        : null)}
-                                </div>
+                                <Collapse bordered={false} defaultActiveKey={['1']}>
+                                    <Panel header=<h2>4 years</h2> key="1">
+                                        <p>All children should receive any missed routine childhood vaccinations. Children who missed their recommended vaccines in childhood can still receive them free under the National Immunisation Program up until they turn 20 years old.
+                                        </p>
+                                    </Panel>
+                                </Collapse>
                             </div>
-
                         </div>
 
 
                         <div className="timelines" style={{ display: 'flex', justifyContent: 'flex-end' }}>
                             <Timeline>
                                 <Timeline.Item>Birth</Timeline.Item>
-                                <Timeline.Item>2 months</Timeline.Item>
-                                <Timeline.Item>4 months</Timeline.Item>
-                                <Timeline.Item>6 months</Timeline.Item>
+                                <Timeline.Item>2-6 months</Timeline.Item>
                                 <Timeline.Item>12 months</Timeline.Item>
                                 <Timeline.Item>18 months</Timeline.Item>
                                 <Timeline.Item>4 years</Timeline.Item>
