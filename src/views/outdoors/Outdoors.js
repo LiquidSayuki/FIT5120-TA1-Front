f5946a22c81d5753f7d5772d8171066f2c1be473
rwan0085 <rwan0085@student.monash.edu>
2022-09-08 17:03:44 +1000

    'Outdoors' quiz setup

--- src/views/outdoors/Outdoors.js
+++ src/views/outdoors/Outdoors.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LocationIdentifier from "../../components/basicPageFrame/LocationIdentifier";
 import { Layout, Col, Row, Divider, Typography, Collapse } from 'antd';
 import './Outdoors.css'
+import Quiz from './Quiz';
 
 const { Content } = Layout;
 const { Title, Paragraph } = Typography;
@@ -29,11 +30,13 @@ const Outdoors = () => {
                     </Row>
                     <div style={{ display: 'flex', justifyContent: 'space-between', paddingTop: "0.5rem" }}>
                         <div style={{ justifyContent: 'center' }}>
-                            <h2>1. Gardening</h2>
-                            <img src='https://s1.imagehub.cc/images/2022/09/07/Family-Planting-in-Garden.jpg.webp' style={{ width: '50%', height: 'auto' }}></img>
+                            {/* <h2>Happy Family Gardening Time</h2> */}
+                            {/* <img src='https://s1.imagehub.cc/images/2022/09/07/Family-Planting-in-Garden.jpg.webp' style={{ width: '50%', height: 'auto' }}></img> */}
+                            <Quiz />
                         </div>
 
-                        <div className="content" style={{ display: 'flex-left' }}>
+                        {/* introduce the potential gardening infections */}
+                        {/* <div className="content" style={{ display: 'flex-left' }}>
                             <div style={{ paddingTop: "30px" }}>
                                 <Collapse bordered={false} defaultActiveKey={['1']}>
                                     <Panel header=<h2>Legionella</h2> key="1">
@@ -42,12 +45,12 @@ const Outdoors = () => {
                                     </Panel>
                                 </Collapse>
                             </div>
-                        </div>
+                        </div> */}
 
                     </div>
 
-                    <h2>2. Swimming</h2>
-                    <h2>3. Sports</h2>
+                    {/* <h2>2. Swimming</h2>
+                    <h2>3. Sports</h2> */}
 
                 </div>
             </div>
---
+++ src/views/outdoors/Quiz.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import './Quiz.css'
+
+export default function Quiz() {
+    const questions = [
+        {
+            questionText: 'Can you tell where the kids ',
+            questionImage: <img src='https://s1.imagehub.cc/images/2022/09/07/Family-Planting-in-Garden.jpg.webp' style={{ width: '70%', height: 'auto' }}></img>,
+            answerOptions: [
+                { answerText: 'Smiling', isCorrect: false },
+                { answerText: 'Gloves', isCorrect: true },
+                { answerText: 'Paris', isCorrect: false },
+                { answerText: 'Dublin', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'Who is CEO of Tesla?',
+            answerOptions: [
+                { answerText: 'Jeff Bezos', isCorrect: false },
+                { answerText: 'Elon Musk', isCorrect: true },
+                { answerText: 'Bill Gates', isCorrect: false },
+                { answerText: 'Tony Stark', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'The iPhone was created by which company?',
+            answerOptions: [
+                { answerText: 'Apple', isCorrect: true },
+                { answerText: 'Intel', isCorrect: false },
+                { answerText: 'Amazon', isCorrect: false },
+                { answerText: 'Microsoft', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'How many Harry Potter books are there?',
+            answerOptions: [
+                { answerText: '1', isCorrect: false },
+                { answerText: '4', isCorrect: false },
+                { answerText: '6', isCorrect: false },
+                { answerText: '7', isCorrect: true },
+            ],
+        },
+    ];
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    const [showScore, setShowScore] = useState(false);
+
+    const [score, setScore] = useState(0);
+
+    const handleAnswerButtonClick = (isCorrect) => {
+
+        if (isCorrect === true) {
+            setScore(score + 1);
+        }
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+        }
+        else {
+            setShowScore(true);
+        }
+    }
+
+    return (
+        <div className='quiz-container'>
+            {/* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+            {showScore ? (
+                <div className='score-section'>You scored {score} out of {questions.length}</div>
+            ) : (
+                <>
+                    <div className='question-section'>
+                        <div className='question-count'>
+                            <span>Question {currentQuestion + 1}</span>/{questions.length}
+                            {/* <span>Happy Family Gardening Time</span> */}
+                        </div>
+                        <div className='question-text'>
+                            {questions[currentQuestion].questionText}
+                            {questions[currentQuestion].questionImage}
+                        </div>
+                    </div>
+                    <div className='answer-section'>
+                        {questions[currentQuestion].answerOptions.map((answerOptions) =>
+                            <button onClick={() => handleAnswerButtonClick(answerOptions.isCorrect)}>{answerOptions.answerText}</button>
+                        )}
+                    </div>
+                </>
+            )}
+        </div>
+    );
+}
